6bd79b56c21cfdd99dcc52eb21a9d4b25171c5e6
ljlclark <35316538+ljlclark@users.noreply.github.com>
2025-04-27 18:18:21 +0800

    HTML Region

--- Apps/Region/Table.js
+++ Apps/Region/Table.js
@@ -23,6 +23,81 @@ class Table
   // Table Functions
   // ***************
 
+  // Get the table column count.
+  static ColumnCount(eTable)
+  {
+    let retCount = 0;
+
+    let fullRow = this.FullRow(eTable);
+    let cells = fullRow.cells;
+    retCount = cells.length;
+    return retCount;
+  }
+
+  // Get the column width.
+  static ColumnWidth(eTable, columnIndex)
+  {
+    let retWidth = 0;
+
+    let rows = eTable.rows;
+    if (rows.Length > 0)
+    {
+      let rowIndex = 0;
+      let cell = this.Cell(eTable, rowIndex, columnIndex);
+      if (cell != null)
+      {
+        retWidth = LJC.ElementStyle(cell, "width");
+      }
+    }
+    return retWidth;
+  }
+
+  // Get row with full column count.
+  static FullRow(eTable)
+  {
+    let retRow = null;
+
+    let prevCount = 0;
+    let rows = eTable.rows;
+    for (let rowIndex = 0; rowIndex < rows.length; rowIndex++)
+    {
+      let row = rows[rowIndex];
+      let rowCellCount = this.RowCellCount(row);
+      if (rowCellCount == prevCount)
+      {
+        retRow = row;
+        break;
+      }
+      prevCount = rowCellCount;
+    }
+    return retRow;
+  }
+
+  // Sets a column width.
+  static SetColumnWidth(eTable, rowIndex, columnIndex, width)
+  {
+    let retWidth = 0;
+
+    let column = this.Cell(eTable, rowIndex, columnIndex);
+    if (column != null)
+    {
+      column.style.width = this.StyleText(width);
+      retWidth = LJC.ElementStyle(column, "width");
+    }
+    return retWidth;
+  }
+
+  // Gets the table width.
+  static Width(eTable)
+  {
+    let retValue = LJC.ElementStyle(eTable, "width");
+    retValue = this.StyleValue(retValue);
+    return retValue;
+  }
+
+  // Add Column Functions
+  // *********************
+
   // Adds a column to the table.
   static AddColumn(eTable)
   {
@@ -53,64 +128,41 @@ class Table
     return retValue;
   }
 
-  // Get the table column count.
-  static ColumnCount(eTable)
+  // Inserts a column into the table.
+  static InsertColumn(eTable, columnIndex)
   {
-    let retCount = 0;
+    let retValue = 0;
 
     let rows = eTable.rows;
-    for (let index = 0; index < rows.length; index++)
+
+    if (rows.length > 0)
     {
-      let row = rows[index];
-      let rowCellCount = this.RowCellCount(row);
-      if (rowCellCount == retCount)
+      for (let index = 0; index < rows.length; index++)
       {
-        retCount = rowCellCount;
-        break;
+        let row = rows[index];
+        let cells = row.cells;
+        if (cells.length < columnIndex + 1)
+        {
+          var cell = cells[cells.length - 1];
+          cell.colSpan += columnIndex - cells.length;
+          continue;
+        }
+        let prevCell = cells[columnIndex];
+        let parent = prevCell.parentElement;
+        let newCell = null;
+        if ("TH" == prevCell.nodeName)
+        {
+          newCell = document.createElement("th");
+          parent.insertBefore(newCell, prevCell);
+        }
+        else
+        {
+          newCell = document.createElement("td");
+          parent.insertBefore(newCell, prevCell);
+        }
+        this.CopyClasses(prevCell, newCell);
       }
-      retCount = rowCellCount;
     }
-    return retCount;
-  }
-
-  // Get the column width.
-  static ColumnWidth(eTable, columnIndex)
-  {
-    let retWidth = 0;
-
-    let rows = eTable.rows;
-    if (rows.Length > 0)
-    {
-      let rowIndex = 0;
-      let cell = this.Cell(eTable, rowIndex, columnIndex);
-      if (cell != null)
-      {
-        retWidth = LJC.ElementStyle(cell, "width");
-      }
-    }
-    return retWidth;
-  }
-
-  // Sets a column width.
-  static SetColumnWidth(eTable, columnIndex, width)
-  {
-    let retWidth = 0;
-
-    let rowIndex = 0;
-    let column = this.Cell(eTable, rowIndex, columnIndex);
-    if (column != null)
-    {
-      column.style.width = this.StyleText(width);
-      retWidth = LJC.ElementStyle(column, "width");
-    }
-    return retWidth;
-  }
-
-  // Gets the table width.
-  static Width(eTable)
-  {
-    let retValue = LJC.ElementStyle(eTable, "width");
-    retValue = this.StyleValue(retValue);
     return retValue;
   }
 
@@ -159,17 +211,19 @@ class Table
   // Style Functions
   // ***************
 
-  static StyleText(value)
+  // Get the style text with added suffix.
+  static StyleText(value, suffix = "px")
   {
     let retText = null;
 
     if (value != null)
     {
-      retText = value + "px";
+      retText = value + suffix;
     }
     return retText;
   }
 
+  // Get the value from the style text.
   static StyleValue(text)
   {
     let retValue = 0;
