ddc0c58a60dc721071cad181ef9537dfe98fca62
ljlclark <35316538+ljlclark@users.noreply.github.com>
2026-04-22 12:07:21 +0800

    Matcher

--- Apps/Matcher/LJCMatcherData.js
+++ Apps/Matcher/LJCMatcherData.js
@@ -14,12 +14,12 @@ class LJCMatcherData
   constructor()
   {
     const retItems = [
-      "1, images/Crossing.jpg, 2, Wear a seat belt while in a car.",
-      "2, images/GreenLight.jpg, 6, Cross the street at the zebra crossing.",
-      "3, images/SeatBelt.jpg, 1, Do not cross between moving vehicles.",
-      "4, images/Helment.jpg, 5, Use the crosswalk and do not jaywalk.",
-      "5, images/Traffic.jpg, 3, Wear a helmet when riding a bicycle.",
-      "6, images/Jwalk.jpg, 4, Wait for the green light before crossing.",
+      "1, Images/Crossing.jpg, 2, Wear a seat belt while in a car.",
+      "2, Images/GreenLight.jpg, 6, Cross the street at the zebra crossing.",
+      "3, Images/SeatBelt.jpg, 1, Do not cross between moving vehicles.",
+      "4, Images/Helment.jpg, 5, Use the crosswalk and do not jaywalk.",
+      "5, Images/Traffic.jpg, 3, Wear a helmet when riding a bicycle.",
+      "6, Images/Jwalk.jpg, 4, Wait for the green light before crossing.",
     ];
     return retItems;
   }
